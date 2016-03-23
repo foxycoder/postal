70c2a6730d63a3fbaab881ed3cd9beeabab07a47
Andrew Harrison <a.git14@uio9.org>
2016-03-23 10:11:22 +0100

    New posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,16 @@ class PostsController < ApplicationController
      @posts = Post.all
   end
 
+  def create
+     post_params = params.require( :post ).permit( :content )
+
+     @post = Post.new( content: post_params[:content] )
+
+     if @post.save
+        redirect_to posts_path
+     else
+        render posts_path
+     end
+  end
+
 end
