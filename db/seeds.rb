b13e1890132a0353454a42079046ebc010ca94f3
Andrew Harrison <a.git14@uio9.org>
2016-03-23 08:26:33 +0100

    Seed Posts.

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Post.create( content: "Pizza is yummy!"                    )
+Post.create( content: "Veggies are tasty and nutritious."  )
+Post.create( content: "Salt is the best ingredient."       )
+
+Post.create( content: "I've seen Star Wars 23 times."      )
+Post.create( content: "Walking Dead has an exciting plot." )
+Post.create( content: "I only watch art-house films."      )
+
+Post.create( content: "My cats are cute!"      )
+Post.create( content: "Dogs are good frieds."  )
+Post.create( content: "My hamster resents me." )
+
+Post.create( content: "Free market solves all problems."         )
+Post.create( content: "Workers control the means of production." )
+Post.create( content: "Can't we all just get along?"             )
