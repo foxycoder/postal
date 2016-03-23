ec63d604db718f1842a7a03e3b64ece1a516a5a7
Andrew Harrison <a.git14@uio9.org>
2016-03-23 16:42:38 +0100

    Remove render.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,11 +4,9 @@ class LikesController < ApplicationController
       post = Post.find( params[:post_id] )
       like = Like.new( post: post, user: current_user )
 
-      if like.save
-         redirect_to posts_path
-      else
-         render posts_path
-      end
+      like.save
+
+      redirect_to posts_path
    end
 
 end
