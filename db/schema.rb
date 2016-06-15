c5a8928c4f778f45b6efb9eff4fbe85ac8ee612f
Wouter de Vos <wouter@codaisseur.com>
2016-06-15 21:35:27 +0200

    Fix windows mess

--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 20160323144106) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
